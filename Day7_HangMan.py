4db37d8c4296ff6ea38e423d3134f2d91db09de5
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2025-01-11 23:19:07 +0200

    Updated Day7_HangMan.py
    Updated HangMan

--- Day7_HangMan.py
+++ Day7_HangMan.py
@@ -59,6 +59,7 @@ word_list = ["aardvark", "baboon", "camel"]
 
 # TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
 #  Set 'lives' to equal 6.
+lives = 6
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
@@ -90,6 +91,11 @@ while not game_over:
 
     # TODO-2: - If guess is not a letter in the chosen_word, Then reduce 'lives' by 1.
     #  If lives goes down to 0 then the game should stop and it should print "You lose."
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            game_over = True
+            print("You lose!")
 
     if "_" not in display:
         game_over = True
@@ -97,3 +103,73 @@ while not game_over:
 
     # TODO-3: - print the ASCII art from 'stages'
     #  that corresponds to the current number of 'lives' the user has remaining.
+
+    if lives == 6:
+        print('''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''')
+    elif lives == 5:
+        print('''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''')
+    elif lives == 4:
+        print('''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''')
+    elif lives == 3:
+        print('''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''')
+    elif lives == 2:
+        print(r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''')
+    elif lives == 1:
+        print(r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''')
+    elif lives == 0:
+        print(r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''')
