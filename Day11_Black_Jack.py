02a464bffb4b4597a0bf80fd342afe22a95f271a
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2025-01-18 22:03:01 +0200

    Update Day11_Black_Jack.py

--- Day11_Black_Jack.py
+++ Day11_Black_Jack.py
@@ -2,57 +2,67 @@ import random
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-funds = 1000
+# funds = 1000
 
-user_list = []
-dealer_list = []
+user_card_list = []
+computer_card_list = []
 
+user_card_init = random.choice(cards)
+computer_cards_init = random.choice(cards)
+
+user_card_list.append(user_card_init)
+computer_card_list.append(computer_cards_init)
+
+def end_game():
+    game = False
+    return game
+
+def deal_cards():
+    user_cards = random.choice(cards)
+    computer_cards = random.choice(cards)
+
+    user_card_list.append(user_cards)
+    computer_card_list.append(computer_cards)
+
+    user_score = 0
+    computer_score = 0
+
+    for score in user_card_list:
+        user_score += score
+
+    for dealer_score in computer_card_list:
+        computer_score += dealer_score
+
+    print("Your cards : ", user_card_list, ", current score is: ", user_score)
+    print("Computer first card : ", computer_card_list[0])
+
+    if user_score > 21 or computer_score > 21:
+        print("Your cards : ", user_card_list, ", final score is: ", user_score)
+        print("Computer final hand is : ", computer_card_list, "final score : ", computer_score)
+        if user_score > computer_score:
+            print("You win!!!")
+        else:
+            print("Computer wins!!!")
+        end_game()
+        user_card_list.clear()
+        computer_card_list.clear()
+
+        user_card = random.choice(cards)
+        computer_cards = random.choice(cards)
+
+        user_card_list.append(user_card_init)
+        computer_card_list.append(computer_cards_init)
 
 def black_jack():
-
-    total_user = 0
-    total_dealer = 0
-    deal = True
-
-    while deal:
-        initial_deal = 2
-        for card in range(0, initial_deal):
-            user_choice = random.choice(cards)
-            dealer_choice = random.choice(cards)
-
-            if total_user <= 21 and total_dealer <= 21:
-                user_list.append(user_choice)
-                dealer_list.append(dealer_choice)
-
-                print("User Cards : ", user_list)
-                print("Dealer Cards : ", dealer_list[0])
-
-                status = input("Hit or Stand ? ")
-                if status == "Hit":
-                    black_jack()
-                else:
-                    for final_user in user_list:
-                        total_user += final_user
-
-                    for final_dealer in dealer_list:
-                        total_dealer += final_dealer
-
-                    if total_dealer > total_user:
-                        print("Dealer wins!")
-                    else:
-                        print("You win!")
+    game = True
+    while game:
+        play_game = input("Do you want to play a game ? 'Y' or 'N' : ")
+        if play_game == "Y":
+            deal_cards()
+            more_card = input("Type 'Y' to get another card or 'N' to pass : ")
+            if more_card == "Y":
+                deal_cards()
             else:
-                print("Total User : ", total_user)
-                print("Total Dealer : ", total_dealer)
-                if total_dealer > total_user:
-                    print("Dealer wins!")
-                else:
-                    print("You win!")
-                    deal = False
+                game = False
 
-amount_to_bet = int(input("Enter amount to bet : "))
-if funds > amount_to_bet:
-    funds -= amount_to_bet
-    black_jack()
-else:
-    print("Insufficient Funds")
+black_jack()
