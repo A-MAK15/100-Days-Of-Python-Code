d7c7870c826018bc21dc4945da6cf903ac0481bd
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2025-01-30 21:59:50 +0200

    Update Day18.py

--- Day18.py
+++ Day18.py
@@ -1,1 +1,50 @@
+from turtle import Turtle, Screen
 
+timmy_the_turtle = Turtle()
+
+timmy_the_turtle.shape("triangle")
+timmy_the_turtle.color("DarkMagenta")
+
+
+for triangle in range(0, 3):
+    timmy_the_turtle.left(120)
+    timmy_the_turtle.forward(100)
+
+for square in range(0, 3):
+    timmy_the_turtle.left(90)
+    timmy_the_turtle.forward(100)
+
+timmy_the_turtle.left(180)
+timmy_the_turtle.forward(100)
+
+# for pentagon in range(0, 4):
+
+timmy_the_turtle.right(200)
+timmy_the_turtle.forward(100)
+
+timmy_the_turtle.left(60)
+timmy_the_turtle.forward(100)
+
+timmy_the_turtle.left(72)
+timmy_the_turtle.forward(100)
+
+timmy_the_turtle.left(72)
+timmy_the_turtle.forward(100)
+
+# timmy_the_turtle.left(60)
+# timmy_the_turtle.forward(100)
+#
+# timmy_the_turtle.left(60)
+# timmy_the_turtle.forward(100)
+#
+# timmy_the_turtle.left(60)
+# timmy_the_turtle.forward(120)
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
