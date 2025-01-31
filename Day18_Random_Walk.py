d2fc80c3bd29a55008dc746b42ccf106b5462452
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2025-01-31 20:15:38 +0200

    Update Day18_Random_Walk.py

--- Day18_Random_Walk.py
+++ Day18_Random_Walk.py
@@ -1,1 +1,18 @@
+from turtle import Turtle, Screen
+import random
 
+timmy_the_turtle = Turtle()
+
+timmy_the_turtle.shape("triangle")
+timmy_the_turtle.color("DarkMagenta")
+
+left = timmy_the_turtle.left(10)
+right = timmy_the_turtle.right(10)
+
+for random_walk in range(0, 50):
+    timmy_the_turtle.forward(10)
+    direction = [left, right]
+    random.choice(direction)
+  
+screen = Screen()
+screen.exitonclick()
