745cbc93f21d9c9a45ec6581e79e1abe6084abb7
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2025-02-01 21:53:44 +0200

    Update Day18_Turtle.py

--- Day18_Turtle.py
+++ Day18_Turtle.py
@@ -1,56 +1,30 @@
 from turtle import Turtle, Screen
+import random
 
 timmy_the_turtle = Turtle()
 
-timmy_the_turtle.shape("triangle")
+# timmy_the_turtle.shape("triangle")
 timmy_the_turtle.color("DarkMagenta")
 
+timmy_the_turtle.pensize(6)
 
-for triangle in range(0, 3):
-    timmy_the_turtle.left(120)
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.color("DarkSeaGreen")
-
-for square in range(0, 3):
+def zigzag(num):
+    timmy_the_turtle.forward(num)
+    timmy_the_turtle.right(90)
+    timmy_the_turtle.forward(num)
     timmy_the_turtle.left(90)
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.color("CadetBlue")
 
-timmy_the_turtle.left(180)
-timmy_the_turtle.forward(100)
+def zongzeng(no):
+    timmy_the_turtle.forward(no)
+    timmy_the_turtle.left(90)
+    timmy_the_turtle.forward(no)
+    timmy_the_turtle.right(90)
 
-timmy_the_turtle.right(200)
-timmy_the_turtle.forward(100)
-
-for pentagon in range(0, 4):
-    timmy_the_turtle.left(72)
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.color("DarkGrey")
-
-for hexagon in range(0, 6):
-    timmy_the_turtle.left(60)
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.color("cyan3")
-
-for heptagon in range(0, 7):
-    timmy_the_turtle.left(51)
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.color("DarkSlateBlue")
-
-for octagon in range(0, 8):
-    timmy_the_turtle.left(45)
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.color("aquamarine4")
-
-for nonagon in range(0, 9):
-    timmy_the_turtle.left(40)
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.color("aquamarine")
-
-for decagon in range(0, 10):
-    timmy_the_turtle.left(36)
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.color("CornFlowerBlue")
+for direction in range(1, 101):
+    random_num = random.randint(1, 100)
+    zigzag(random_num)
+    zongzeng(random_num)
+    timmy_the_turtle.left(90)
 
 screen = Screen()
 screen.exitonclick()
