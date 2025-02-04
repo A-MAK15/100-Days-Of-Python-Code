4504258e127cf1ea10ee344375ed1b86437ef896
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2025-02-04 10:07:45 +0200

    Update Day18_Spirograph.py

--- Day18_Spirograph.py
+++ Day18_Spirograph.py
@@ -1,1 +1,31 @@
+from turtle import Turtle, Screen
+import random
 
+screen = Screen()
+screen.colormode(255)
+
+timmy_the_turtle = Turtle()
+
+# timmy_the_turtle.shape("triangle")
+timmy_the_turtle.color("DarkMagenta")
+
+timmy_the_turtle.home()
+
+timmy_the_turtle.circle(100)
+
+def random_colour():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    rgb = (r, g, b)
+    return rgb
+
+timmy_the_turtle.speed("fastest")
+
+for circle in range(int(360/10)):
+    timmy_the_turtle.color(random_colour())
+    timmy_the_turtle.circle(100)
+    timmy_the_turtle.right(10)
+
+screen = Screen()
+screen.exitonclick()
