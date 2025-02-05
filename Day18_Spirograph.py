59ad8df4802675b04d92e8bc4c8e2fa40a1f5999
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2025-02-05 07:00:20 +0200

    Update Day18_Spirograph.py

--- Day18_Spirograph.py
+++ Day18_Spirograph.py
@@ -33,31 +33,37 @@ screen.exitonclick()
 ## Hirst
 
 from turtle import Turtle, Screen
+import random
 
 tim = Turtle()
+
+screen = Screen()
+screen.colormode(255)
 
 color_list = [(223, 232, 241), (245, 231, 238), (231, 243, 236), (204, 159, 107), (231, 213, 109), (134, 168, 192), (44, 105, 144), (152, 78, 53), (199, 142, 164), (15, 32, 53), (181, 159, 42), (148, 65, 87), (141, 178, 155), (205, 91, 70), (64, 117, 92), (195, 89, 118), (225, 170, 187), (15, 38, 27), (59, 34, 19)]
 
 row_length = 0
 row_height = 0
+teleportation = 0
 
 def dot_draw():
-    tim.dot(20, "red")
+    tim.dot(20, random.choice(color_list))
     tim.penup()
     tim.forward(100)
 
 def row():
     global row_length
     while row_length < 5:
+        # random_color =
         dot_draw()
         row_length += 1
-    tim.teleport(0, 70)
-    dot_draw()
 
 while row_height < 5:
     row()
-    dot_draw()
+    teleportation += 70
+    tim.teleport(0, teleportation)
     row_height += 1
+    row_length = 0
 
 
 screen = Screen()
