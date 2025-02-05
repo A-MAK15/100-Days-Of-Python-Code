a2049597a9671bc336ca402445f7aa7c9cdc5e77
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2025-02-05 18:51:56 +0200

    Update Day19.py

--- Day19.py
+++ Day19.py
@@ -28,3 +28,44 @@ screen.onkey(key="d", fun=clockwise)
 screen.onkey(key="c", fun=clear_drawing)
 
 screen.exitonclick()
+
+## Turtle race
+from turtle import Turtle, Screen
+import random
+
+screen = Screen()
+screen.setup(width=500, height=400)
+color = ["red", "orange", "yellow", "green", "blue", "purple"]
+is_race_on = False
+all_turtles = []
+
+user_bet = screen.textinput(title="Place your bet", prompt="Which turtle will win the race ? Enter color : ")
+# print(user_input)
+
+y = -100
+
+for turtle in range(6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(color[turtle])
+    new_turtle.penup()
+    new_turtle.goto(x=-238, y=y)
+    all_turtles.append(new_turtle)
+    y += 40
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won!!!!The {winning_color} turtle is the winner")
+            else:
+                print(f"You have lost. The {winning_color} turtle won")
+        rand_distance = random.randint(1,10)
+        turtle.forward(rand_distance)
+
+
+screen.exitonclick()
