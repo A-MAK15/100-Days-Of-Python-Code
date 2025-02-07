8b0aa0f588d80db23dd53fc868b5fde4c7faa888
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2025-02-07 22:34:29 +0200

    Update Snake_Part1_Day20.py

--- Snake_Part1_Day20.py
+++ Snake_Part1_Day20.py
@@ -11,6 +11,12 @@ screen.tracer(0)
 
 snake = Snake()
 
+screen.listen()
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+
 game_is_on = True
 while game_is_on:
     screen.update()
@@ -21,10 +27,11 @@ while game_is_on:
 screen.exitonclick()
 
 #snake
-from turtle import Turtle
+from turtle import Turtle, Screen
 
-# POSITIONS = [(0,0), (-20, 0), (-40, 0)]
+POSITIONS = [(0,0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 30
+screen = Screen()
 
 class Snake:
 
@@ -34,20 +41,32 @@ class Snake:
         self.snake_creation()
 
     def snake_creation(self):
-        for _ in range(3):
+        for coor in POSITIONS:
             new_turtle = Turtle("square")
             new_turtle.penup()
             new_turtle.color("white")
-            new_turtle.goto(self.x_position, 0)
+            new_turtle.goto(coor)
             self.snakes.append(new_turtle)
-            self.x_position -= 20
+            # self.x_position -= 20
 
     def move(self):
         for snake_num in range(len(self.snakes) - 1, 0, -1):
             new_x = self.snakes[snake_num - 1].xcor()
             new_y = self.snakes[snake_num - 1].ycor()
             self.snakes[snake_num].goto(new_x, new_y)
-        self.snakes[0].forward(MOVE_DISTANCE)
+        self.snakes[0].forward(10)
+
+    def up(self):
+        self.snakes[0].setheading(90)
+
+    def down(self):
+        self.snakes[0].setheading(270)
+
+    def left(self):
+        self.snakes[0].setheading(180)
+
+    def right(self):
+        self.snakes[0].setheading(0)
 
 
 
