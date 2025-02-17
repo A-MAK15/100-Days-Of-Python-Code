a45e208ef463017ba24644eaaeed0041e652aeab
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2025-02-17 17:00:40 +0200

    Update Day21_Snake_Part2.py

--- Day21_Snake_Part2.py
+++ Day21_Snake_Part2.py
@@ -1,1 +1,50 @@
+# Scorebard CLass
+from turtle import Turtle
 
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        # self.write("Home = Score", True, align="center")
+
+    def screen_write(self):
+        self.write("Score = ", True, align="center")
+        # self.write((0, 0), True)
+# Main
+from turtle import Screen
+import time
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+
+score = scoreboard.score
+# print(score)
+# scoreboard.write(score)
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        score += 1
+        print(score)
+
+screen.exitonclick()
