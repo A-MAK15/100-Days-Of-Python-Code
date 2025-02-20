18f9d1855bd10ad2d442cca924b49ce1cec58e8a
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2025-02-20 18:14:11 +0200

    Update Day22_ping_pong.py

--- Day22_ping_pong.py
+++ Day22_ping_pong.py
@@ -3,6 +3,7 @@ from turtle import Screen
 from paddles import Paddles
 from ball import Ball
 import time
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.bgcolor("black")
@@ -13,64 +14,35 @@ screen.tracer(0)
 r_paddle = Paddles((370,0))
 l_paddle = Paddles((-370,0))
 ball = Ball()
+score = Scoreboard()
 
+screen.listen()
 screen.onkey(fun=r_paddle.up, key="Up")
 screen.onkey(fun=r_paddle.down, key="Down")
 screen.onkey(fun=l_paddle.up, key="w")
 screen.onkey(fun=l_paddle.down, key="s")
 game_is_on = True
 
+
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.ball_speed)
     screen.update()
     ball.move()
 
+    if ball.ycor() > 280.0 or ball.ycor() < -280.0:
+        ball.bounce_y()
 
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+     #r_paddle miss
+    if ball.xcor() > 380:
+        ball.reset_position()
+        score.l_point()
+
+    # l_paddle miss
+    if ball.xcor() < -380:
+        ball.reset_position()
+        score.r_point()
 
 screen.exitonclick()
-
-#paddles
-from turtle import Turtle
-
-UP = 90
-DOWN = -270
-
-class Paddles(Turtle):
-    def __init__(self, position):
-        super().__init__()
-        self.shape("square")
-        self.color("white")
-        self.shapesize(stretch_wid=5, stretch_len=1)
-        self.penup()
-        self.goto(position)
-
-    def up(self):
-        y_pos = self.ycor() + 20
-        self.goto(x=self.xcor(),y=y_pos)
-
-    def down(self):
-        y_pos = self.ycor() - 20
-        self.goto(x=self.xcor(), y=y_pos)
-
-#ball
-from turtle import Turtle
-
-class Ball(Turtle):
-    def __init__(self):
-        super().__init__()
-        self.shape("circle")
-        self.color("white")
-        self.penup()
-        # self.goto(x=380, y=288)
-
-    def move(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
-
-        # self.goto(new_x, new_y)
-
-        while self.ycor() != 590:
-            self.goto(new_x, new_y)
-        # self.goto(self.xcor() - 20, self.ycor() - 20)
-        self.right(20)
-        self.goto(new_x - 10, new_y - 10)
