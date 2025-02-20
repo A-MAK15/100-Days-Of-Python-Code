5747e895155d146610d2993098cc99949be2e609
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2025-02-20 18:15:08 +0200

    Update Day22_ping_pong.py

--- Day22_ping_pong.py
+++ Day22_ping_pong.py
@@ -46,3 +46,36 @@ while game_is_on:
         score.r_point()
 
 screen.exitonclick()
+
+#paddles
+
+#ball
+
+#scoreboard
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+        self.updated_scoreboard()
+
+    def updated_scoreboard(self):
+        self.clear()
+        self.goto(-100, 200)
+        self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
+        self.goto(100, 200)
+        self.write(self.r_score, align="center", font=("Courier", 80, "normal"))
+
+    def l_point(self):
+        self.l_score += 1
+        self.updated_scoreboard()
+
+    def r_point(self):
+        self.r_score += 1
+        self.updated_scoreboard()
+
