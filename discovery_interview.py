44d5af43d9804025677f444ac77eef0aeaeb15bf
Aphiwe Makwelo <72697191+A-MAK15@users.noreply.github.com>
2025-02-24 19:14:29 +0200

    Update discovery_interview.py

--- discovery_interview.py
+++ discovery_interview.py
@@ -1,1 +1,15 @@
+import pandas as pd
+import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
+
+meeting_instances = pd.read_csv("meeting_instances.csv")
+meeting_instances
+
+meeting_metadata = pd.read_csv("meeting_metadata.csv")
+meeting_metadata
+
+person_info = pd.read_csv("person_info.csv")
+person_info
+
 
